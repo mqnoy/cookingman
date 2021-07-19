84fa7503e0a6d0e259ef5c33484c7c462b905b10
mqnoy <qnoy.rifky@gmail.com>
2021-07-19 23:15:09 +0700

    initial commit

---
+++ android/app/src/main/kotlin/com/example/cookingman/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.cookingman
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/utils/decoration/colors.dart
@@ -0,0 +1,5 @@
+import 'package:flutter/material.dart';
+
+const Color black2e2e2e = Color(0xff2E2E2E);
+const Color yellowd8a107 = Color(0xffD8A107);
+const Color silvere3e3e3 = Color(0xffE3E3E3);
